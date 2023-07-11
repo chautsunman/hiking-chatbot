117c0337c7c70714143a28338f0d078540055ac4
Man Chau <tsunman.chau.g@gmail.com>
2023-07-11 10:13:42 +0800

    Hiking chatbot

---
+++ hiking_chatbot.py
@@ -0,0 +1,31 @@
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+from langchain import HuggingFaceHub
+from langchain import PromptTemplate, LLMChain
+from langchain.llms import GPT4All
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+st.title("Hiking Chatbot")
+prompt_txt = st.text_input("Prompt")
+
+repo_id = "google/flan-t5-xxl"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
+llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 64})
+
+template = """{prompt}"""
+prompt = PromptTemplate.from_template(template=template)
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+if prompt_txt:
+    print(f"running - prompt: {prompt_txt}")
+
+    res = llm_chain.run(prompt_txt)
+
+    st.write(res)
